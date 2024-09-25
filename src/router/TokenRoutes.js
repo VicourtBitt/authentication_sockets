fce0ec239d4397ec8de696f809dd73087274861a
VicourtBitt <victorgcb@outlook.com>
2024-09-25 10:43:48 -0300

    Some instructions

--- src/router/TokenRoutes.js
+++ src/router/TokenRoutes.js
@@ -9,4 +9,11 @@ routes.post('/token', tokenController.createTokens);
 routes.post('/token/access', tokenController.createAccessToken);
 routes.patch('/token', tokenController.updateRefreshTokenInDB);
 
+// To make this works, you'll need to insert the cookie on the "client-side", then
+// it'll be visible in the request/response header
+routes.get('/test', (req, res) => {
+    // if (!req.headers['set-cookie']) return res.status(401).json({ message: 'No Cookie Registered' });
+    return res.status(200).json({ message: res.headers});
+});
+
 module.exports = routes;
