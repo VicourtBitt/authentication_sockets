b710d9f4083097693abc6393a8b5a009fcf56580
VicourtBitt <victorgcb@outlook.com>
2024-09-30 21:47:24 -0300

    add/fix: Resolving authentication problems

--- src/router/CredentialRoutes.js
+++ src/router/CredentialRoutes.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Router } from 'express';
-import authenticateJWT from '../utils/authenticateJWT.js';
+import { authenticateJWT } from '../utils/authenticateJWT.js';
 import CredentialController from '../controller/CredentialController.js';
 const credentialController = new CredentialController();
 
@@ -10,8 +10,9 @@ const routes = Router();
 
 routes.get(`${process.env.GET_EXISTING_CREDENTIALS}`, credentialController.credentialAlreadyExists);
 
-// Probably we won't need this route, but it's here just in case
-// routes.post('/credential', credentialController.createCredential);
+// This route should only be used in debug/development mode, it'll create a new credential
+// and serve as component testing
+// routes.post(`${process.env.POST_CREDENTIAL}`, authenticateJWT, credentialController.createCredential);
 
 // The routes below are protected by the JWT
 routes.patch(`${process.env.PATCH_CREDENTIALS}`, authenticateJWT, credentialController.updatePassword);
