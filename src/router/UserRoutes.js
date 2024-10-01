f516e27973fc6da6cef5f089ab447dce0e49b14e
VicourtBitt <victorgcb@outlook.com>
2024-09-30 21:57:53 -0300

    add/feat: Development/Debbuging function

--- src/router/UserRoutes.js
+++ src/router/UserRoutes.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { Router } from 'express';
 import UserController from '../controller/UserController.js';
-import authenticateJWT from '../utils/authenticateJWT.js';
+import { authenticateJWT } from '../utils/authenticateJWT.js';
 
 const userController = new UserController();
 const routes = Router();
@@ -10,7 +10,9 @@ const routes = Router();
 
 dotenv.config();
 
-routes.post(`${process.env.POST_ONLY_USER}`, userController.createUser);
+// This route should only be used in debug/development mode, it'll create a new user
+// routes.post(`${process.env.POST_ONLY_USER}`, userController.createUser);
+
 routes.post(`${process.env.POST_USER}`, userController.createFullRegister);
 routes.get(`${process.env.GET_USER}`, userController.findUserById);
 routes.get(`${process.env.GET_ALL_USERS}`, userController.findAllUsers);
