77cfd58eac42380a8834246aa3c28d005cdd958e
VicourtBitt <victorgcb@outlook.com>
2024-09-30 21:40:39 -0300

    doc/feat: Updating Token Authentication and using JSDocs

--- src/services/TokenService.js
+++ src/services/TokenService.js
@@ -5,25 +5,48 @@ import jwt from 'jsonwebtoken'
 import { prisma } from '../config/database.js'
 
 class TokenService {
-    // This function will create both access and refresh tokens, it'll
-    // be used when the user logs in the application
+    
+    /**
+     * This function will create both tokens, both will be stored as httpOnly cookies
+     * (refresh token will be stored in the database as well). It'll access the prisma
+     * model and check if the user already has a refresh token, if it does, it'll return
+     * the access token and the refresh token. If it doesn't, it'll create a new refresh
+     * token and return both.
+     * 
+     * @param {object} data This is the data object that will be used to create the tokens
+     * @param {string} data.name This is the name of the current user trying to auth
+     * @param {number} data.userId This is the userId of the current user trying to auth
+     * 
+     * @returns
+     */
     async createTokens (data) {
         const { name, userId } = data
-        const refreshToken = await this.createRefreshToken({name, userId})
-
-        const refresh = await prisma.UserToken.create({
-            data: {
-                refreshToken: refreshToken,
+        const alreadyExists = await prisma.userToken.findUnique({
+            where: {
                 userId: userId
             }
         })
-
+        if (alreadyExists) {
+            const { refreshToken } = alreadyExists
+            return { accessToken: await this.createAccessToken({name, userId}), refreshToken }
+        }
+        const refreshToken = await this.createRefreshToken({name, userId})
         const accessToken = await this.createAccessToken({name, userId})
-        return accessToken
+        return { accessToken, refreshToken }
     }
 
-    // This function will create a new access token, it'll be used when
-    // the user's access token expires
+    /**
+     * This function will create a new access token, it'll be used when the user 
+     * need to authenticate itself. It'll use the name and userId to create a secret
+     * string, and then it'll create the token with the secret string and the JWT
+     * access string key.
+     * 
+     * @param {object} data This is the data object that will be used to create the access token
+     * @param {string} data.name This is the name of the current user trying to auth
+     * @param {number} data.userId This is the userId of the current user trying to auth
+     * 
+     * @returns 
+     */
     async createAccessToken (data) {
         const { name, userId } = data
         const userSecret = name.replace(' ', '') + userId
@@ -33,10 +56,23 @@ class TokenService {
         return jwt.sign({ userSecret }, secretString, { expiresIn: '30m' })
     }
 
+    /**
+     * This function will verify the refresh token, it'll be used when the user
+     * needs to authenticate itself. It'll check if the token is valid, if it's
+     * not, it'll return false. If it is, it'll check if the token is expired,
+     * if it is, it'll update the token in the database and return false. If it's
+     * valid, it'll return true.
+     * 
+     * @param {*} data
+     * @param {string} data.token
+     * @param {number} data.userId
+     *  
+     * @returns 
+     */
     async verifyRefreshToken (data) {
         const { token, userId } = data
         const currentTime = new Date(Date.now())
-        const userToken = await prisma.UserToken.findUnique({
+        const userToken = await prisma.userToken.findUnique({
             where: {
                 userId: userId
             }
@@ -47,24 +83,42 @@ class TokenService {
             await this.updateRefreshTokenInDB({ name: userToken.userId, userId, token })
             return false
         }
+        return true
     }
 
-    // This function will create a new refresh token, it'll be used when
+    /**
+     * It's basically the same function as createAccessToken, but it'll create
+     * a new refresh token.
+     * 
+     * @param {*} data
+     * @param {string} data.name
+     * @param {number} data.userId
+     * 
+     * @returns 
+     */
     async createRefreshToken (data) {
         const { name, userId } = data
         const userSecret = name.replace(' ', '') + userId
-
         const secretString = process.env.JWT_REFRESH_STRING_KEY
 
         return jwt.sign({ userSecret }, secretString, { expiresIn: '2d' })
     }
 
-    // This function will update the refresh token in the database, it'll be
-    // used when the user's refresh token expires
+    /**
+     * This function will update the refresh token in the database, it'll be used
+     * when the user needs to authenticate and the refresh token has expired.
+     * 
+     * @param {*} data 
+     * @param {string} data.name
+     * @param {number} data.userId
+     * @param {string} data.token
+     * 
+     * @returns 
+     */
     async updateRefreshTokenInDB (data) {
         const { name, userId, token } = data
 
-        const update = await prisma.UserToken.update({
+        const update = await prisma.userToken.update({
             where: {
                 refreshToken: token
             },
