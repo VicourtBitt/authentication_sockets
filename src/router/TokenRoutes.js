6bb1f566dd9f063d3264be879bb9c2a0cea77c9f
VicourtBitt <victorgcb@outlook.com>
2024-09-30 21:46:21 -0300

    add/fix: New Routes with Enviroment Variables and AuthenticationJWT

--- src/router/TokenRoutes.js
+++ src/router/TokenRoutes.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import TokenController from '../controller/TokenController.js';
-import authenticateJWT from '../utils/authenticateJWT.js';
+import { authenticateJWT } from '../utils/authenticateJWT.js';
 import { Router } from 'express';
 
 dotenv.config();
@@ -8,14 +8,7 @@ const tokenController = new TokenController();
 const routes = Router();
 
 routes.post(`${process.env.POST_NEW_LOGIN}`, tokenController.createTokens);
-routes.post(`${process.env.POST_NEW_ACCESS}`, authenticateJWT,tokenController.createAccessToken);
-routes.patch(`${process.env.PATCH_NEW_REFRESH}`, authenticateJWT,tokenController.updateRefreshTokenInDB);
-
-// To make this works, you'll need to insert the cookie on the "client-side", then
-// it'll be visible in the request/response header
-routes.get('/test', (req, res) => {
-    // if (!req.headers['set-cookie']) return res.status(401).json({ message: 'No Cookie Registered' });
-    return res.status(200).json({ message: res.header});
-});
+routes.post(`${process.env.POST_NEW_ACCESS}`, authenticateJWT, tokenController.createAccessToken);
+routes.patch(`${process.env.PATCH_NEW_REFRESH}`, authenticateJWT, tokenController.updateRefreshTokenInDB);
 
 export default routes;
