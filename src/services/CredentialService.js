9aa9cb776a52681ff714fe77a15981bae11b12b1
VicourtBitt <victorgcb@outlook.com>
2024-09-30 21:53:48 -0300

    doc/feat: JSDoc and New Functions

--- src/services/CredentialService.js
+++ src/services/CredentialService.js
@@ -1,22 +1,44 @@
 import { prisma } from '../config/database.js'
 
-// Trying to use Classes without the need to instantiate the prisma object
+
+/**
+ * CredentialService is a class that will handle the creation, deletion and update of the user credentials.
+ * 
+ * @function createCredential This function will create a new user credential.
+ * @function credentialAlreadyExists This function will check if the credentials already exists in our database
+ * @function updatePassword This function will update the password credentials of the user
+ * @function deleteCredential This function will delete the credentials of the user
+ */
 class CredentialService {
-    // This route will create the credentials, which will be used in
-    // the User Controller as well.
+
+    /**
+     * This function will create a new user credential.
+     * 
+     * @param {object} data This is the data object that will be used to create the credentials
+     * @param {string} data.email This is the user email
+     * @param {string} data.password This is the user hashed password (for security reasons)
+     * @param {number} data.userId This is the userId of the current user trying to auth
+     *  
+     * @returns {Object} This function will return the new user credentials
+     */
     async createCredential (data) {
         const { email, password, userId } = data
-        return prisma.credentials.create({
+        const newUser = prisma.credentials.create({
             data: {
                 email,
                 password,
                 userId
             }
         })
+        return newUser
     }
 
-    // This route will check if the credentials already exists in
-    // our database
+    /**
+     * Will verify if the credentials already exists in our database
+     * 
+     * @param {*} email The email of the user
+     * @returns 
+     */
     async credentialAlreadyExists (email) {
         const credential = await prisma.credentials.findUnique({
             where: { email }
@@ -24,7 +46,15 @@ class CredentialService {
         return credential
     }
 
-    // This route will update the password credentials of the user
+    /**
+     * Will update the password of the user
+     * 
+     * @param {object} data Objects passed to the function
+     * @param {string} data.email User email
+     * @param {string} data.password User hashed password
+     * 
+     * @returns 
+     */
     async updatePassword (data) {
         const { email, password } = data
         return prisma.credentials.update({
@@ -33,7 +63,13 @@ class CredentialService {
         })
     }
 
-    // This route will delete the credentials of the user
+    /**
+     * Will delete the credentials of the user
+     * 
+     * @param {string} email The email of the user
+     * 
+     * @returns 
+     */
     async deleteCredential (email) {
         return prisma.credentials.delete({
             where: { email }
